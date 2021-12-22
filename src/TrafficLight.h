118039abc65a9f592aaefcaf5b83446e4728e9d0
shubham92000 <sbhsingh616@gmail.com>
2021-12-22 23:56:55 +0530

    added TrafficLight Class

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -30,7 +30,13 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+enum TrafficLightPhase {
+    red ,
+    green
+};
+
+
+class TrafficLight : public TrafficObject
 {
 public:
     // constructor / desctructor
@@ -38,9 +44,15 @@ public:
     // getters / setters
 
     // typical behaviour methods
+    void waitForGreen() ;
+    void simulate() ;
+    TrafficLightPhase getCurrentPhase() ;
 
 private:
     // typical behaviour methods
+    void cycleThroughPhases() ;
+
+    TrafficLightPhase _currentPhase ;
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
